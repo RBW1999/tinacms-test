ecfdf8afcc84edf8ca01d494836e5166cb25e4e2
Raimund Canzler <raimund.canzler@t-online.de>
2021-05-17 00:18:27 +0200

    Make repo public

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,25 @@
-import App from 'next/app'
-import '../styles/globals.css';
-import { TinaCMS, TinaProvider } from 'tinacms'
+import App from "next/app";
+import "../styles/globals.css";
+import { TinaCMS, TinaProvider } from "tinacms";
 import {
   GithubClient,
   TinacmsGithubProvider,
   GithubMediaStore,
-} from 'react-tinacms-github'
+} from "react-tinacms-github";
 
 export default class Site extends App {
-  cms: TinaCMS
+  cms: TinaCMS;
 
   constructor(props) {
-    super(props)
+    super(props);
 
     const github = new GithubClient({
-      proxy: '/api/proxy-github',
-      authCallbackRoute: '/api/create-github-access-token',
+      proxy: "/api/proxy-github",
+      authCallbackRoute: "/api/create-github-access-token",
       clientId: process.env.GITHUB_CLIENT_ID,
       baseRepoFullName: process.env.REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
       baseBranch: process.env.BASE_BRANCH, // e.g. 'master' or 'main' on newer repos
-      authScope: 'repo' // private repo
-    })
+    });
 
     /**
      * 1. Create the TinaCMS instance
@@ -42,11 +41,11 @@ export default class Site extends App {
        */
       sidebar: props.pageProps.preview,
       toolbar: props.pageProps.preview,
-    })
+    });
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       /**
        * 5. Wrap the page Component with the Tina and Github providers
@@ -64,39 +63,39 @@ export default class Site extends App {
           <Component {...pageProps} />
         </TinacmsGithubProvider>
       </TinaProvider>
-    )
+    );
   }
 }
 
 const onLogin = async () => {
-  const token = localStorage.getItem('tinacms-github-token') || null
-  const headers = new Headers()
+  const token = localStorage.getItem("tinacms-github-token") || null;
+  const headers = new Headers();
 
   if (token) {
-    headers.append('Authorization', 'Bearer ' + token)
+    headers.append("Authorization", "Bearer " + token);
   }
 
-  const resp = await fetch(`/api/preview`, { headers: headers })
-  const data = await resp.json()
+  const resp = await fetch(`/api/preview`, { headers: headers });
+  const data = await resp.json();
 
-  if (resp.status == 200) window.location.href = window.location.pathname
-  else throw new Error(data.message)
-}
+  if (resp.status == 200) window.location.href = window.location.pathname;
+  else throw new Error(data.message);
+};
 
 const onLogout = () => {
   return fetch(`/api/reset-preview`).then(() => {
-    window.location.reload()
-  })
-}
+    window.location.reload();
+  });
+};
 
 export interface EditLinkProps {
-  cms: TinaCMS
+  cms: TinaCMS;
 }
 
 export const EditLink = ({ cms }: EditLinkProps) => {
   return (
     <button onClick={() => cms.toggle()}>
-      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+      {cms.enabled ? "Exit Edit Mode" : "Edit This Site"}
     </button>
-  )
-}
+  );
+};
